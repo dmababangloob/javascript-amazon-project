a773b79e2ea275ed7b4ad39c6b89c8560d1e5ed4
dmababangloob <dmn.mababangloob@gmail.com>
2024-12-11 20:11:59 +0800

    Make delivery options interactive

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import { cart, removeFromCart } from '../data/cart.js';
+import { cart, removeFromCart, updateDeliveryOption } from '../data/cart.js';
 import { products } from '../data/products.js';
 import { formatCurrency } from './utils/money.js';
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js'
@@ -32,13 +32,12 @@ cart.forEach((cartItem) => {
     });
 
     const today = dayjs();
-          const deliveryDate = today.add(
-            deliveryOption.deliveryDays, 'days'
-          );
-          const dateString = deliveryDate.format(
-            'dddd, MMMM D'
-          );
-    
+    const deliveryDate = today.add(
+      deliveryOption.deliveryDays, 'days'
+    );
+    const dateString = deliveryDate.format(
+      'dddd, MMMM D'
+    );
        
     cartSummaryHTML +=`
      <div class="cart-item-container 
@@ -102,17 +101,19 @@ cart.forEach((cartItem) => {
           const isChecked = deliveryOption.id === cartItem.deliveryOptionId;
 
          html += ` 
-              <div class="delivery-option">
+              <div class="delivery-option js-delivery-option"
+              data-product-id="${matchingProduct.id}"
+              data-delivery-option-id="${deliveryOption.id}">
                   <input type="radio"
-                  ${isChecked ?'checked' : ''}
-                  class="delivery-option-input"
-                  name="${matchingProduct.id}">
+                    ${isChecked ?'checked' : ''}
+                    class="delivery-option-input"
+                    name="delivery-option-${matchingProduct.id}">
                 <div>
                   <div class="delivery-option-date">
                     ${dateString}
                   </div>
                   <div class="delivery-option-price">
-                  ${priceString} Shipping
+                    ${priceString} Shipping
                   </div>
                 </div>
               </div>
@@ -136,3 +137,11 @@ cart.forEach((cartItem) => {
             container.remove();
           });
       });
+
+    document.querySelectorAll('.js-delivery-option')
+        .forEach((element) => {
+          element.addEventListener('click', () => {
+            const {productId, deliveryOptionId} = element.dataset;
+            updateDeliveryOption(productId, deliveryOptionId);
+          });
+        });
