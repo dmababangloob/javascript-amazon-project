870242e81de4a5517e70ef9892fd7325673005cc
dmababangloob <dmn.mababangloob@gmail.com>
2024-12-17 14:30:39 +0800

    Convert from promises to async await

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -5,6 +5,23 @@ import { loadCart } from "../data/cart.js";
 //import '../data/cart-class.js';
 //import '../data/backend-practice.js'
 
+async function loadPage(params) {
+
+  await loadProductsFetch();
+
+  await new Promise((resolve) => {
+    loadCart(() => { 
+      resolve(); 
+    });
+  });
+
+  renderOrderSummary();
+  renderPaymentSummary();
+
+}
+loadPage();
+
+/*
 Promise.all([
   loadProductsFetch(),
   new Promise((resolve) => {
@@ -18,7 +35,7 @@ Promise.all([
   renderPaymentSummary();
 });
 
-/*
+
 new Promise((resolve) => { 
   loadProducts(() => { 
    resolve('value1'); 
