79babf736f8cd0ff4799d136bde2e7dd84e1f755
dmababangloob <dmn.mababangloob@gmail.com>
2024-12-17 15:10:42 +0800

    Error handling

--- data/products.js
+++ data/products.js
@@ -72,6 +72,8 @@ export function loadProductsFetch() {
         return new Product(productDetails); 
     });
       console.log('load products');
+  }).catch((error) => {
+    console.log('Unexpected error. Please try again later.');
   });
 
   return promise;
@@ -97,11 +99,13 @@ export function loadProducts(fun) {
       fun();
   });
 
+  xhr.addEventListener('error', (error) => {
+    console.log('Unexpected error. Please try again later.');
+  })
   
   xhr.open('GET', 'https://supersimplebackend.dev/products');
   xhr.send();
 }
-
 
 /*
 export const products = [
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -5,19 +5,23 @@ import { loadCart } from "../data/cart.js";
 //import '../data/cart-class.js';
 //import '../data/backend-practice.js'
 
-async function loadPage(params) {
+async function loadPage() {
+  try {
+      //throw 'error1';
+      
+    await loadProductsFetch();
 
-  await loadProductsFetch();
-
-  await new Promise((resolve) => {
-    loadCart(() => { 
-      resolve(); 
+    const value = await new Promise((resolve) => {
+      loadCart(() => { 
+        resolve(); 
+      });
     });
-  });
+  } catch (error) {
+    console.log('Unexpected error. Please try again later.');
+  } 
 
   renderOrderSummary();
   renderPaymentSummary();
-
 }
 loadPage();
 
